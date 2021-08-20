6a829bb19dcc0966cfa8f235dd00a61e5b0425e5
Mayank Goel <goel.mayank235@gmail.com>
2021-08-21 03:44:04 +0530

    Second commit

--- numbersystem.js
+++ numbersystem.js
@@ -3,10 +3,10 @@ var ans = [];
 
 let i = 0;
 while (num != 1) {
-    ans[i] = num % 2;
+    ans.push(num % 2);
     num = num / 2;
 }
-ans[i] = num % 2;
+ans.push(num % 2);
 for (an of ans) {
     console.log(an);
 }
