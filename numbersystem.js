ebf3cc5bd49c244eb1872be9d7f41e90f9c076c5
Mayank Goel <goel.mayank235@gmail.com>
2021-08-21 04:11:43 +0530

    third commit

--- numbersystem.js
+++ numbersystem.js
@@ -1,12 +1,15 @@
-let num = document.querySelector('#numin');
-var ans = [];
+console.log("HI");
 
-let i = 0;
+let num = document.querySelector('.numin');
+
+var ans = [0, 0, 0, 0, 0, 0];
+
+var i = 0;
 while (num != 1) {
-    ans.push(num % 2);
+    ans[i] = (num % 2);
     num = num / 2;
 }
-ans.push(num % 2);
+ans[i] = (num % 2);
 for (an of ans) {
     console.log(an);
 }
