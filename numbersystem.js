d64f0739ef222a2550fdc6064146127804cee8de
Mayank Goel <goel.mayank235@gmail.com>
2021-09-03 01:41:44 +0530

    Figured how to get binary form

--- numbersystem.js
+++ numbersystem.js
@@ -1,15 +1,6 @@
 console.log("HI");
 
-let num = document.querySelector('.numin');
-
-var ans = [0, 0, 0, 0, 0, 0];
-
-var i = 0;
-while (num != 1) {
-    ans[i] = (num % 2);
-    num = num / 2;
-}
-ans[i] = (num % 2);
-for (i = 0; i < 6; i++) {
-    console.log(ans[i]);
-}
+let ma = 56
+const anss = document.querySelector('.ans');
+let answer = ma.toString(2);
+anss.innerHTML = answer;
