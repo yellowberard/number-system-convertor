3c7d113622f7eeb91edc854fd0925bdb131d5268
Mayank Goel <goel.mayank235@gmail.com>
2021-09-05 03:37:17 +0530

    Sixth Commit

--- numbersystem.js
+++ numbersystem.js
@@ -1,13 +1,21 @@
 console.log("HI");
 const button = document.querySelector('.btn');
+const ocpara = document.querySelector('.op');
+const bipara = document.querySelector('.bp');
+const hexpara = document.querySelector('.hp');
 
-const anss = document.querySelector('.ans');
 button.addEventListener('click', function (evt) {
     var num = document.getElementById("myNumber").value;
-    let an = num;
+    let temp = num;
 
-    console.log(num);
-    let ab = (an >>> 0).toString(2);
-    console.log(ab);
-    anss.textContent = ab;
+    let bians = (temp >>> 0).toString(2);
+    bipara.textContent = bians;
+
+    let octans = (temp >>> 0).toString(8);
+    ocpara.textContent = octans;
+
+    let hexans = (temp >>> 0).toString(16);
+    hexpara.textContent = hexans;
+
+
 });
