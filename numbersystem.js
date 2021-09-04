0a705324608148636b6891ca0069947ebe109a63
Mayank Goel <goel.mayank235@gmail.com>
2021-09-04 14:39:44 +0530

    fifth commit

--- numbersystem.js
+++ numbersystem.js
@@ -1,10 +1,30 @@
 console.log("HI");
 const button = document.querySelector('.btn');
 var num = document.getElementById("myNumber").value;
+let an = num;
 
-const anss = document.querySelector('.ans');
+// const anss = document.querySelector('.ans');
 button.addEventListener('click', function (evt) {
-
-    let answer = num.toString(2);
-    anss.innerHTML = answer;
+    console.log(num);
+    let ab = (an >>> 0).toString(2);
+    console.log(ab);
 });
+
+function myFunction() {
+    var x = document.getElementById("myNumber").value;
+    // console.log(x);
+    let test = x;
+    let answer = (test >>> 0).toString(2);
+    console.log(answer);
+    document.getElementById("demo").innerHTML = answer;
+
+}
+
+function testing() {
+    var x = document.getElementById("myNumber").value;
+    // console.log(x);
+    let test = x;
+    let answer = (test >>> 0).toString(2);
+    console.log(answer);
+    document.getElementById("demo").innerHTML = answer;
+}
